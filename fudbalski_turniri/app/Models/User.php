fb205ee18e7cd7e4d42a8fe339de5e08b283ebdb
AleksandarStanojevic00 <stanojevicaleksandar035@gmail.com>
2024-05-14 18:37:18 +0200

    Izvrsene su porebne promene na modelu User

--- fudbalski_turniri/app/Models/User.php
+++ fudbalski_turniri/app/Models/User.php
@@ -44,4 +44,8 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+    public function tournaments()
+    {
+        return $this->hasMany(Tournament::class);
+    }
 }
