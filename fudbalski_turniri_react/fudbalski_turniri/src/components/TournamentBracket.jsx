38c8a7e11830e71e19aa8d9ddc67b9088a7ed03f
aleksandar-radin <ar20190328@student.fon.bg.ac.rs>
2024-06-23 20:25:28 +0200

    dodata strana za brackete i css za turnir stranu

---
+++ fudbalski_turniri_react/fudbalski_turniri/src/components/TournamentBracket.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import classes from "./TournamentsBracket.module.css";
+
+const FootballTournamentBracket = () => {
+  return (
+    <div className={classes.bracket}>
+      {/* Round 1 */}
+      <div className={classes.round}>
+        <div className={classes.match}>
+          <div className={classes.team}>Team A</div>
+          <div className={classes.team}>Team B</div>
+        </div>
+        <div className={classes.match}>
+          <div className={classes.team}>Team C</div>
+          <div className={classes.team}>Team D</div>
+        </div>
+      </div>
+
+      {/* Round 2 */}
+      <div className={classes.round}>
+        <div className={classes.match}>
+          <div className={classes.team}>Winner of Match 1</div>
+          <div className={classes.team}>Winner of Match 2</div>
+        </div>
+      </div>
+
+      {/* Round 3 (Final) */}
+      <div className={classes.round}>
+        <div className={classes.match}>
+          <div className={classes.team}>Finalist 1</div>
+          <div className={classes.team}>Finalist 2</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FootballTournamentBracket;
