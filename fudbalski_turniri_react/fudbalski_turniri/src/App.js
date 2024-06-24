98658685a5b49fbd03d78f292950229855d34f76
aleksandar-radin <ar20190328@student.fon.bg.ac.rs>
2024-06-24 12:17:43 +0200

    privremeno namesteni bracket-i

--- fudbalski_turniri_react/fudbalski_turniri/src/App.js
+++ fudbalski_turniri_react/fudbalski_turniri/src/App.js
@@ -45,6 +45,34 @@ const AppContent = () => {
   },
 ]);
 
+
+const [teamsData] = useState([
+  { id: 1, name: 'Italy', points: 9 },
+  { id: 2, name: 'Switzerland', points: 4 },
+  { id: 3, name: 'Turkey', points: 1 },
+  { id: 4, name: 'Wales', points: 4 },
+  { id: 5, name: 'Belgium', points: 9 },
+  { id: 6, name: 'Denmark', points: 3 },
+  { id: 7, name: 'Finland', points: 3 },
+  { id: 8, name: 'Russia', points: 3 },
+  { id: 9, name: 'Austria', points: 6 },
+  { id: 10, name: 'Netherlands', points: 9 },
+  { id: 11, name: 'North Macedonia', points: 0 },
+  { id: 12, name: 'Ukraine', points: 3 },
+  { id: 13, name: 'Croatia', points: 4 },
+  { id: 14, name: 'Czech Republic', points: 4 },
+  { id: 15, name: 'England', points: 7 },
+  { id: 16, name: 'Scotland', points: 1 },
+  { id: 17, name: 'Poland', points: 1 },
+  { id: 18, name: 'Slovakia', points: 3 },
+  { id: 19, name: 'Spain', points: 5 },
+  { id: 20, name: 'Sweden', points: 7 },
+  { id: 21, name: 'France', points: 5 },
+  { id: 22, name: 'Germany', points: 4 },
+  { id: 23, name: 'Hungary', points: 2 },
+  { id: 24, name: 'Portugal', points: 4 }
+]);
+
   const location = useLocation();
   const hideNavbar = location.pathname === '/';
 
@@ -56,7 +84,7 @@ const AppContent = () => {
         <Route path="/contact" element={<Contact />} />
         <Route path="/about" element={<About />} />
         <Route path="/tournaments" element={<Tournaments allTournaments = {allTournaments} />} />
-        <Route path="/tournament-bracket" element={<TournamentBracket/>} />
+        <Route path="/tournament-bracket" element={<TournamentBracket teamsData = {teamsData} />} />
       </Routes>
     </div>
   );
